8b9a482baa63948b7556d2227140ea3efcea5501
vertefra <verte.fra@gmail.com>
2020-10-07 13:45:03 -0400

    first test with fiber

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/vertefra/go-fiber-tutorial
+
+go 1.13
+
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/fiber/v2 v2.0.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello World")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", helloWorld)
+	app.Listen(3000)
+}
